e59ecefbbd7f709584437c14c0caa513ee9b0065
Leticiacruz166 <leticia_cruz16@hotmail.com>
2025-08-06 21:40:29 -0300

    Add files via upload
    01/08/2025

---
+++ exercicios 1.2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   int numero, quadrado;
+   printf("informe um numero:");
+   scanf("%d", &numero);
+   quadrado = numero * numero;
+   printf("o quadrado do numero eh: %d\n", quadrado);
+    return 0;
+}
