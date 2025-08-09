a40411b67f7fc75e9fd56ef2971d69366c745b55
Leticiacruz166 <leticia_cruz16@hotmail.com>
2025-08-08 21:18:47 -0300

    08/08/2025

---
+++ Exercicio 2.3/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("informe um valor:");
+    scanf("%d", &num);
+    //num = num + 1;
+    //num += 1; num é o mumero real onde é acrescentado +1
+    //num++ ou ++num o num soma + 1 se tornado no mumero real +1
+    printf("novo valor: %d\n", num);
+    return 0;
+}
